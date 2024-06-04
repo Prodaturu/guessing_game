553c2c0afbdb8e3086c2411f3208fccf54d9c3a2
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-06-04 22:01:01 +0200

    added spaces after comments for better readability

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 02:09:45 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/06/04 21:45:06 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/06/04 21:59:57 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,27 @@ int	main()
 	int	secret_num;
 
 	/* Seed the random number generator */
+
 	srand(time(NULL));
 	sleep(1);
 
 	/* Generate a random number between 1 and 10 */
+
 	secret_num = rand() % 10 + 1;
 	guess_limit = 4;
 	retry_limit = 3;
 
 	/* Loop until the user guesses the correct number */
+
 	while (guess_limit > 0 && retry_limit > 0)
 	{
 		/* Prompt the user to guess a number */
+
 		printf("Guess a number between 1 and 10: ");
 		scanf("%d", &guess_num);
 
 		/* Check if the user guessed the correct number */
+		
 		if (guess_num >10 || guess_num < 1)
 		{
 			printf("You guessed a number out of range, retry!\n");
